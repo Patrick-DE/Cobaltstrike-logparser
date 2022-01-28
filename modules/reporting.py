0142a03dd5ba282c10e66c158ae09536fb1a0900
Patrick E (Sh4d0w) <Patrick-DE@users.noreply.github.com>
2022-01-28 13:48:09 +0100

    added some additional reporting

--- modules/reporting.py
+++ modules/reporting.py
@@ -1,12 +1,14 @@
 from modules.sql.sqlite_func import *
 from modules.utils import *
+from modules.ttp import *
 
 def sort_on_timestamp(elem: Entry):
     return elem.timestamp
+
 def sort_on_joined(elem: Beacon):
     return elem.joined
 
-def report_input_task(o):
+def report_input_task(output):
     # input report
     entries = get_all_entries_filtered(filter=EntryType.input)
     entries = entries + get_all_entries_filtered(filter=EntryType.task)
@@ -15,24 +17,40 @@ def report_input_task(o):
     for entry in entries:
         rows.append(entry.to_row())
     header = ["Date", "Time", "Hostname", "Command", "User", "IP"]
-    write_to_csv(o+"\\activity-report.csv", header, rows)
+    write_to_csv(output+"\\activity-report.csv", header, rows)
 
-def report_dl_ul(o):
-    # get download and upload report
-    entries = get_all_entries_filtered(filter=EntryType.download)
+def report_dl_ul(output):
+    """
+    get download and upload report
+    """
+    entries = get_all_entries_filtered_containing(filter=EntryType.task, cont="Tasked beacon to download")
     entries = entries + get_upload_entries()
     entries.sort(key=sort_on_timestamp)
     rows = []
     for entry in entries:
         rows.append(entry.to_row())
     header = ["Date", "Time", "Hostname", "File", "User", "IP"]
-    write_to_csv(o+"\\dl-ul-report.csv", header, rows)
+    write_to_csv(output+"\\dl-ul-report.csv", header, rows)
 
-def report_all_beacons_spawned(o):
-    entries = get_all_complete_beacons()
-    entries.sort(key=sort_on_joined)
+def report_all_beacons_spawned(output):
+    beacons = get_all_valid_beacons()
+    beacons.sort(key=sort_on_joined)
+    rows = []
+    for beacon in beacons:
+        rows.append(beacon.to_row())
+    header = ["Hostname", "IP", "Internet via IP", "User", "Process", "Process ID", "Joined", "Exited"]
+    write_to_csv(output+"\\beacon-report.csv", header, rows)
+
+def report_all_indicators(output):
+    """
+    get download and upload report
+    """
+    entries = get_all_entries_filtered(filter=EntryType.indicator)
+    entries.sort(key=sort_on_timestamp)
     rows = []
     for entry in entries:
         rows.append(entry.to_row())
-    header = ["Hostname", "IP", "Internet via IP", "User", "Process", "Process ID", "Joined", "Exited"]
-    write_to_csv(o+"\\beacon-report.csv", header, rows)
+    header = ["Date", "Time", "Hostname", "File", "User", "IP"]
+    write_to_csv(output+"\\ioc-report.csv", header, rows)
+
+
